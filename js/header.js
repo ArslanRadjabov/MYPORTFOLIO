a83f62719d03b4a31bd856f4f0b84e2323a5d775
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-02-14 14:49:25 +0300

    portfolio header hero first section end

---
+++ js/header.js
@@ -0,0 +1,12 @@
+
+let elOpenMenu = document.querySelector('.header__menu');
+let elModal = document.getElementById('navbar');
+
+elOpenMenu.addEventListener('click', function () {
+    elModal.classList.toggle('show');
+    elOpenMenu.classList.toggle('active');  
+
+  });
+  
+  
+
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// let elopenmenu = document.getElementById('menu-btn');
+// let elModal = document.getElementById('navbar');
+
+
+// elopenmenu.addEventListener('click', function () {
+//   elModal.classList.add('show');
+//   elopenmenu.classList.add('show');  
+// });
+
+// elColsemodal.addEventListener('click', function () {
+//   elModal.classList.remove('show');
+// });
+
+
+
+
+
+var typed = new Typed(".type", {
+  strings: [ "Frontend developer","Freelancer","UI designer"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
