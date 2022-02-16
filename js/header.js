20812c8e69ae9f6f417ec067e70384ede4960230
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-02-16 08:21:23 +0500

    sss

--- js/header.js
+++ js/header.js
@@ -9,4 +9,8 @@ elOpenMenu.addEventListener('click', function () {
   });
   
   
-
+window.addEventListener('scroll', function(){
+  let header = this.document.querySelector('header');
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle('scrolling-active',windowPosition);
+})
